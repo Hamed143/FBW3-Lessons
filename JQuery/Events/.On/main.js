fd5a258afc8919ad354c0735412f56981f646d76
Hamed143 <hamed_hossainy@yahoo.com>
2019-09-04 14:06:53 +0200

    all projects

---
+++ JQuery/Events/.On/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    // $("p").on("mouseout mouseover",function(e){
+    //     $('this').toggleClass("light");
+    // });
+    $('p').on({
+       mouseover:function(){
+           $('body').css('background','lightgreen');
+       },
+       mouseout:function(){
+           $('body').css('background','lightblue');
+       },
+       click:function(){
+           $('body').css('background','orange');
+       }
+    });
+    $('div').on('click',"section",function(){
+       $(this).css({"color":"red",
+                     "background":"pink"
+                   }
+           );
+       $(this).slideToggle(1000);
+    });
+    $('#add').on("click",function(){
+       $("<section> this is new section inseted from jQuery </section>").insertAfter("#add");
+    });
+    $("#off").on("click",function(){
+        // remove the event click for the div
+        $("div").off("click");
+    });
+
+
+    });
