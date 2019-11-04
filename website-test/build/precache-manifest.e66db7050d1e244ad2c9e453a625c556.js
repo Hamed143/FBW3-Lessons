a3b37f2a5438459bfa4f8cbfac5f369bc922e3da
Hamed143 <hamed_hossainy@yahoo.com>
2019-11-04 14:38:50 +0100

    commit

---
+++ website-test/build/precache-manifest.e66db7050d1e244ad2c9e453a625c556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4db6b09157cecdfc4b9667683acf8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee96c54a6dbed360f22",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "359ace3032c95558600d",
+    "url": "/static/js/2.143d97ab.chunk.js"
+  },
+  {
+    "revision": "aee96c54a6dbed360f22",
+    "url": "/static/js/main.beb02391.chunk.js"
+  },
+  {
+    "revision": "4cee3e2501ef6fd3d8fb",
+    "url": "/static/js/runtime-main.54710776.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
